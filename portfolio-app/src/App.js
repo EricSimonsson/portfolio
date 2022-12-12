e2dd48d99e49590963e0055b95431af92fe79752
EricSimonson <cirre96@gmail.com>
2022-12-12 14:30:30 +0100

    react app deployed
    Trying to get the deployment of the app look like the react app does when running it locally

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,17 +1,39 @@
 import './App.css';
+import React from 'react';
+import { Route, Link } from "react-router-dom";
+import About from './About';
+import Home from './Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          This is the start of my portfolio
-
-          Hello Hello
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+      return (
+        <div className="App">
+          <div>
+            <nav>
+              <ul id="navigation">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                <Link to="/about">About</Link>
+                </li>
+                <li>
+                <Link to="/contact">Contact</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/about">
+                <About />
+              </Route>
+            </Switch>
+          </div>
+            );
+  }
 }
 
 export default App;
---
+++ portfolio-app/src/Home.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                <p>
+                    Home page
+                </p>
+                </header>
+            </div>
+            
+        );
+    }
+
+}
+
+export default Home;
